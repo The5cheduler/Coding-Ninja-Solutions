009d0c321ce45a579f18e406303ca6459e7ae8cd
Pk-ThinkLP <parthkumar.patel@thinklp.com>
2022-03-23 01:35:23 -0400

    Complete method for recursive

--- SameTree (LeetCode - 100).py
+++ SameTree (LeetCode - 100).py
@@ -30,7 +30,7 @@ class Solution:
             queue.extend([(p.left,q.left),(p.right,q.right)])
         return True
 
-    # Recursive Method
+    # Recursive Method ( O(n) -> where n i number od edges)
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p and q:
-            return not p 
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
