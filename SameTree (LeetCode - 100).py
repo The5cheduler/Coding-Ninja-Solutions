401e0382dad5ecc39d788d34ca3c3ac2b91f4b5f
Pk-ThinkLP <parthkumar.patel@thinklp.com>
2022-03-22 21:41:07 -0400

    Added the BFS Method

--- SameTree (LeetCode - 100).py
+++ SameTree (LeetCode - 100).py
@@ -1,10 +1,11 @@
+import queue
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-#DFS Way
+#DFS Way (Run Time - O(n), Space Time - Constant O(1))
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         stack = [(p,q)]
@@ -16,3 +17,16 @@ class Solution:
                 return False
             stack.extend([(p.left,q.left),(p.right,q.right)])
         return True
+
+#BFS way (Run Time - O(n), Space Time - Constant O(1))
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        queue = queue.dequeue([p, q])
+        while queue:
+            p,q = queue.popleft()
+            if not p and not q:
+                continue
+            if (not p or not q) or (p.val != q.val):
+                return False
+            queue.extend([(p.left,q.left),(p.right,q.right)])
+        return True
+
